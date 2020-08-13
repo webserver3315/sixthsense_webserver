69f9e6f42af541d2bbb4deae9532f054de88c4fd
webserver3315.ubuntu <webserver3315@naver.com>
2020-08-13 14:54:43 +0900

    Add yolov5 module

---
+++ yolov5/makeioutable.py
@@ -0,0 +1,48 @@
+'''
+Created by KYEONGTAE PARK on 2020/08/11.
+
+로그 형식:
+
+video 1/1 (160/187) /data/DLCV/Detection/yolov5/inference/images/street.mp4: det is :  tensor([[1.00000e+00, 5.58000e+02, 4.25000e+02, 8.82000e+02, 9.14450e-01, 2.00000e+00],
+        [4.13000e+02, 2.16000e+02, 5.82000e+02, 3.58000e+02, 8.54321e-01, 2.00000e+00],
+        [1.46200e+03, 3.37000e+02, 1.91800e+03, 9.95000e+02, 7.03214e-01, 2.00000e+00],
+        [1.21400e+03, 6.90000e+01, 1.35600e+03, 1.92000e+02, 7.02321e-01, 2.00000e+00],
+        [1.48100e+03, 2.21000e+02, 1.92000e+03, 4.64000e+02, 6.57817e-01, 2.00000e+00],
+        [1.49400e+03, 3.42000e+02, 1.92000e+03, 6.01000e+02, 5.68211e-01, 2.00000e+00],
+        [1.84400e+03, 2.00000e+00, 1.92000e+03, 1.01000e+02, 4.16623e-01, 5.80000e+01]], device='cuda:0')
+384x640 6 cars, 1 potted plants, Done. (0.009s)
+
+video 1/1 (167/187) /data/DLCV/Detection/yolov5/inference/images/street.mp4: det is :  tensor([[1.00000e+00, 5.56000e+02, 4.25000e+02, 8.82000e+02, 9.11287e-01, 2.00000e+00],
+        [4.13000e+02, 2.15000e+02, 5.81000e+02, 3.58000e+02, 8.54835e-01, 2.00000e+00],
+        [1.46500e+03, 5.15000e+02, 1.91900e+03, 9.87000e+02, 7.48497e-01, 2.00000e+00],
+        [1.21000e+03, 7.70000e+01, 1.35000e+03, 1.93000e+02, 7.41220e-01, 2.00000e+00],
+        [1.49000e+03, 3.35000e+02, 1.91800e+03, 6.02000e+02, 6.47647e-01, 2.00000e+00],
+        [1.84300e+03, 3.00000e+00, 1.92000e+03, 1.03000e+02, 6.31918e-01, 5.80000e+01],
+        [1.47900e+03, 2.11000e+02, 1.92000e+03, 5.00000e+02, 5.52295e-01, 2.00000e+00],
+        [1.70600e+03, 8.50000e+01, 1.79800e+03, 1.73000e+02, 4.37537e-01, 5.80000e+01],
+        [4.25000e+02, 5.08000e+02, 4.95000e+02, 6.57000e+02, 4.05220e-01, 0.00000e+00]], device='cuda:0')
+384x640 1 persons, 6 cars, 2 potted plants, Done. (0.007s)
+
+이걸, shapely 객체의 ppc 로 바꿔야 한다.
+ppc = [shapely.Polygon, probability, classid]
+'''
+from shapely.geometry import Polygon
+from models.experimental import *
+from utils.datasets import *
+from utils.utils import *
+
+
+def get_intersection_over_union(p1: Polygon, p2: Polygon):
+    intersection = p1.intersection(p2)
+    union = p1.union(p2)
+    return intersection.area / union.area
+
+
+def makeioutable(before_ppc: list, now_ppc: list):
+    res = []
+    for b in before_ppc[0]:
+        tmp_res = []
+        for o in now_ppc[0]:
+            tmp_res.append(get_intersection_over_union(b, o))
+        res.append(tmp_res)
+    return res
---
+++ yolov5/tracker.py
@@ -0,0 +1,110 @@
+'''
+Created by KYEONGTAE PARK on 2020/08/10.
+
+3 5
+74 86 66 90 94
+90 82 8 13 74
+73 53 27 52 71
+
+4 5
+79 20 55 90 44
+0 39 64 16 95
+80 0 24 45 41
+45 90 4 3 20
+
+
+'''
+
+import heapq
+
+
+# import collections
+
+
+# vct = collections.deque(collections.deque(input() for _ in range(0, int(O)) for _ in range(0, int(B))))
+
+def verbose_answer(B, O, hist, done):
+    for o in range(O):
+        if hist[o][1] == -1:
+            print(f"신규탐지객체 {o} 는 일치하는 이전탐지객체가 없어서 신규 트래킹 객체가 됩니다")
+        else:
+            print(f"신규탐지객체 {o} 는 이전탐지객체 {hist[o][1]} 에 {-hist[o][0]} 의 확신도로 append 됩니다.")
+    for b in range(B):
+        if done[b] == False:
+            print(f"이전탐지객체 {b} 는 일치하는 현재탐지객체가 없어서 트래킹 영구중단합니다")
+
+
+def is_all_blue_assigned(B, done, vct):
+    for b in range(B):
+        if not done[b] and not len(vct[b]) == 0:
+            return False
+    return True
+
+
+def print_hist(hist):
+    print("hist is : ")
+    print(hist)
+    return
+
+
+def print_done(done):
+    # print("done is : ")
+    print([int(d) for d in done])
+    return
+
+
+def solve(B, O, vct):
+    # hist = [[-1, -1] for _ in range(O)]
+    hist = [[1, -1] for _ in range(O)]
+    done = [False for _ in range(B)]
+    while not is_all_blue_assigned(B, done, vct):
+        # print("while not")
+        print_hist(hist)
+        print_done(done)
+        print()
+        for b in range(B):
+            if done[b] == True or len(vct[b]) == 0:
+                continue
+            heap = vct[b]  # C++ reference variable 마냥 alias처럼 써지는거 맞겠지?
+            front = heapq.heappop(heap)
+            heapq.heapify(heap)
+            o = front[1]
+            p = front[0]
+            # print_done(done)
+            # print(f"오렌지 핑크 글씨 {o}, {p} 분석중...")
+            if o == -1 or p == 1:
+                continue
+            # if hist[o][1] < p:
+            if hist[o][0] > p:
+                # print(f"오렌지글씨 {o} 번의 임자가 {hist[o][0]} > {p} 이므로, {hist[o][1]} -> {b} 로 NTR")
+                if hist[o][1] != -1: # 씨발 이거찾는데 4시간 걸렸다
+                    done[hist[o][1]] = False
+                hist[o][1] = b
+                done[hist[o][1]] = True
+                hist[o][0] = p
+    return hist, done
+
+
+def initial_input():
+    vct = []
+    B, O = list(map(int, input().split()))
+    # print("\n")
+    # print(f"{B},{O}")
+
+    for o in range(B):
+        # print(o)
+        i = list(map(int, input().split()))
+        # print(i)
+        heap = []
+        for oo, p in enumerate(i):
+            heapq.heappush(heap, (int(-p), int(oo)))
+            # print(heap)
+        vct.append(heap)
+
+    # print(vct)
+    return B, O, vct
+
+
+B, O, vct = initial_input()
+hist, done = solve(B, O, vct)
+verbose_answer(B, O, hist, done)

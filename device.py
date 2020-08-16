acef1ac493983d75c1eabfbf817ca7fe7aa04332
Myeongwon Choi <auspic7@gmail.com>
2020-08-16 23:44:18 +0900

    Add function new frame to update object

--- device.py
+++ device.py
@@ -1,5 +1,11 @@
+from yolov5.detect_photo_version2 import get_detected_image_from_photo
+
 class Device:
     def __init__(self, id):
         self.id = id
-        self.frames = list()
-        self.image = None
+        self.objects_list = []
+    
+    def new_frame(self, image_path, timestamp):
+        self.objects_list = get_detected_image_from_photo(image_path, tracking_object_list=self.objects_list)
+        return self.objects_list
+        

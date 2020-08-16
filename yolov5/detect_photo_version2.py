72de1565fb91166bc93811c221b4caf9c7e563bc
Myeongwon Choi <auspic7@gmail.com>
2020-08-16 17:32:42 +0900

    Add path to use yolov5 module

--- yolov5/detect_photo_version2.py
+++ yolov5/detect_photo_version2.py
@@ -1,5 +1,8 @@
 import argparse
-
+import os
+import sys
+# print(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 from tracker import *
 from makeioutable import *
 from xyxypc2ppc import *
@@ -21,7 +24,7 @@ Output: 사진 내부에서 검출된 모든 Object 에 대한 정보 -> trackin
 
 
 # def get_detected_image_from_photo(source, weights, tracking_object_list=[]):
-def get_detected_image_from_photo(source, weights, tracking_object_list=[]):
+def get_detected_image_from_photo(source, weights='yolov5s.pt', tracking_object_list=[]):
     with torch.no_grad():
         print("detect_photo function Start!")
         # out, source, weights, view_img, save_txt, imgsz = \
